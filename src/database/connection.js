86b33e02e5326cc7ce7fcd576e80d83cad4aa768
mlucasdev <lucasmsantos819@gmail.com>
2022-06-20 17:53:31 -0300

    config ssl

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,19 +4,19 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const connection = new Sequelize(
-    // process.env.DATABASE_URL,
-    process.env.DB_BASE,
-    process.env.DB_USER,
-    process.env.DB_PASS,
+    process.env.DATABASE_URL,
+    // process.env.DB_BASE,
+    // process.env.DB_USER,
+    // process.env.DB_PASS,
     {
-        host: process.env.DB_HOST,
-        port: 5432,
+        // host: process.env.DB_HOST,
+        // port: 5432,
         dialect: 'postgres',
-        // dialectOptions: {
-        //     ssl: {
-        //         require: true,
-        //         rejectUnauthorized: false
-        //     }
-        // }
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     }
 );
